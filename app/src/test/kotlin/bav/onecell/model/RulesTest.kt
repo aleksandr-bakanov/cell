2230d16b0c07ac15eefc4e84c0fc204c3fb3579f
Alexander Bakanov <bakanov.aleksandr@gmail.com>
2018-03-22 23:55:05 +0300

    Add more tests

--- app/src/test/kotlin/bav/onecell/model/RulesTest.kt
+++ app/src/test/kotlin/bav/onecell/model/RulesTest.kt
@@ -61,4 +61,19 @@ class RulesTest {
         cell.hexes.add(Hex(1, 0, -1).withType(Hex.Type.ENERGY))
         assertFalse(rules.isAllowedToAddHexIntoCell(cell, Hex(1, -1, 0).withType(Hex.Type.LIFE)))
     }
+
+    @Test
+    fun addLifeHexToEnergyNeighborShouldReturnTrue() {
+        cell.hexes.add(Hex(0, 0, 0).withType(Hex.Type.LIFE))
+        cell.hexes.add(Hex(1, -1, 0).withType(Hex.Type.ENERGY))
+        assertTrue(rules.isAllowedToAddHexIntoCell(cell, Hex(2, -2, 0).withType(Hex.Type.LIFE)))
+    }
+
+    @Test
+    fun addLifeHexWithoutLifeOrEnergyNeighborsShouldReturnFalse() {
+        cell.hexes.add(Hex(0, 0, 0).withType(Hex.Type.LIFE))
+        cell.hexes.add(Hex(1, -1, 0).withType(Hex.Type.ENERGY))
+        cell.hexes.add(Hex(2, -2, 0).withType(Hex.Type.ATTACK))
+        assertFalse(rules.isAllowedToAddHexIntoCell(cell, Hex(3, -3, 0).withType(Hex.Type.LIFE)))
+    }
 }
