b5d187b9e98e7897d08e1b1feb5f61fc8494fdeb
Aleksandr Bakanov <aleksandr_bakanov@epam.com>
2018-05-10 18:27:26 +0300

    Add highlighting to cell list

--- app/src/main/kotlin/bav/onecell/main/CellRecyclerViewAdapter.kt
+++ app/src/main/kotlin/bav/onecell/main/CellRecyclerViewAdapter.kt
@@ -1,11 +1,11 @@
 package bav.onecell.main
 
+import android.graphics.Color
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import bav.onecell.R
-
 import kotlinx.android.synthetic.main.item_row_cell.view.buttonEditCell
 import kotlinx.android.synthetic.main.item_row_cell.view.buttonEditCellRules
 import kotlinx.android.synthetic.main.item_row_cell.view.buttonRemoveCell
@@ -14,10 +14,21 @@ import kotlinx.android.synthetic.main.item_row_cell.view.title
 class CellRecyclerViewAdapter(private val presenter: Main.Presenter) :
         RecyclerView.Adapter<CellRecyclerViewAdapter.ViewHolder>() {
 
+    private var selectedItemPosition = RecyclerView.NO_POSITION
+    private val notifyItemChanged = { index: Int, vh: ViewHolder ->
+        if (vh.adapterPosition == RecyclerView.NO_POSITION) {
+            // do nothing
+        } else {
+            notifyItemChanged(selectedItemPosition)
+            notifyItemChanged(index)
+            selectedItemPosition = index
+        }
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context)
                 .inflate(R.layout.item_row_cell, parent, false)
-        return ViewHolder(view, presenter)
+        return ViewHolder(view, presenter, notifyItemChanged)
     }
 
     override fun getItemCount() = presenter.cellsCount()
@@ -25,15 +36,27 @@ class CellRecyclerViewAdapter(private val presenter: Main.Presenter) :
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.setCellTitle("Cell #$position")
         holder.index = position
+        holder.view.setBackgroundColor(if (selectedItemPosition == position) Color.GREEN else Color.TRANSPARENT)
     }
 
-    class ViewHolder(val view: View, private val presenter: Main.Presenter) : RecyclerView.ViewHolder(view) {
+    class ViewHolder(val view: View, private val presenter: Main.Presenter,
+                     private val notifyItemChanged: (Int, ViewHolder) -> Unit) : RecyclerView.ViewHolder(view) {
+        // TODO: looks like index and adapterPosition are the same
         var index: Int = 0
 
         init {
-            view.buttonEditCell.setOnClickListener { presenter.openCellEditor(index) }
-            view.buttonEditCellRules.setOnClickListener { presenter.openCellRulesEditor(index) }
-            view.buttonRemoveCell.setOnClickListener { presenter.removeCell(index) }
+            view.buttonEditCell.setOnClickListener {
+                presenter.openCellEditor(index)
+                notifyItemChanged(adapterPosition, this)
+            }
+            view.buttonEditCellRules.setOnClickListener {
+                presenter.openCellRulesEditor(index)
+                notifyItemChanged(adapterPosition, this)
+            }
+            view.buttonRemoveCell.setOnClickListener {
+                presenter.removeCell(index)
+                notifyItemChanged(adapterPosition, this)
+            }
         }
 
         fun setCellTitle(title: String) {
