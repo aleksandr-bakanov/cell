2bec47b7f30bbbd9393983cd8c209f077335ed6b
Aleksandr Bakanov <aleksandr_bakanov@epam.com>
2018-12-04 19:07:37 +0300

    Start changing of battle results view

--- app/src/main/kotlin/bav/onecell/battle/results/BattleResultsPresenter.kt
+++ app/src/main/kotlin/bav/onecell/battle/results/BattleResultsPresenter.kt
@@ -13,22 +13,28 @@ class BattleResultsPresenter(
         private val resourceProvider: Common.ResourceProvider,
         private val dealtDamage: MutableMap<Int, Int> = mutableMapOf(),
         private val deadOrAliveCells: MutableMap<Int, Boolean> = mutableMapOf(),
-        private val cellIndexes: MutableList<Int> = mutableListOf()) : BattleResults.Presenter {
+        private val cellIndexes: MutableMap<Int, MutableList<Int>> = mutableMapOf()) : BattleResults.Presenter {
 
     override fun initialize(dealtDamage: Map<Int, Int>, deadOrAliveCells: Map<Int, Boolean>) {
-        cellIndexes.addAll(dealtDamage.keys.sorted())
+        dealtDamage.keys.groupBy { cellRepo.getCell(it)?.data?.groupId }.forEach { entry ->
+            entry.key?.let { groupId ->
+                val indexes = mutableListOf<Int>()
+                indexes.addAll(entry.value.sorted())
+                cellIndexes[groupId] = indexes
+            }
+        }
         this.dealtDamage.putAll(dealtDamage)
         this.deadOrAliveCells.putAll(deadOrAliveCells)
         // TODO: convert dealt damage to money for each cell
     }
 
-    override fun cellsCount(): Int = deadOrAliveCells.size
+    override fun cellsCount(groupId: Int): Int = deadOrAliveCells.size
 
-    override fun getCell(position: Int): Cell? = cellRepo.getCell(cellIndexes[position])
+    override fun getCell(groupId: Int, position: Int): Cell? = cellRepo.getCell(cellIndexes[groupId]?.get(position) ?: -1)
 
-    override fun getDealtDamage(position: Int): Int = dealtDamage[cellIndexes[position]] ?: 0
+    override fun getDealtDamage(groupId: Int, position: Int): Int = dealtDamage[cellIndexes[groupId]?.get(position) ?: -1] ?: 0
 
-    override fun getDeadOrAlive(position: Int): Boolean = deadOrAliveCells[cellIndexes[position]] ?: false
+    override fun getDeadOrAlive(groupId: Int, position: Int): Boolean = deadOrAliveCells[cellIndexes[groupId]?.get(position) ?: -1] ?: false
 
     override fun goToHeroesScreen() {
         router.goToMain()
